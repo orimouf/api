7aaaa6480f9d36f5c3c3d97e654eb0ab46a4eab7
Amir PC <orim.ouf@gmail.com>
2023-06-25 22:50:06 +0200

    update 2.0 25/06/2023

--- routes/appdata.js
+++ routes/appdata.js
@@ -22,23 +22,30 @@ router.post("/dataclients", async (req, res) => {
 
         if (idCheck != null) {
             try {
-                const updatedClient = await Client.findByIdAndUpdate(idCheck._id, 
-                    {
-                        appId: Element.id,
-                        clientName: Element.client_name,
-                        phone: Element.phone,
-                        region: Element.region,
-                        prices: Element.prices,
-                        oldCredit: Element.old_credit,
-                        isCredit: is_credit,
-                        isFrigo: is_frigo,
-                        isPromo: is_promo,
-                        creditBon: Element.credit_bon,
-                        lastServe: Element.last_serve
-                    },
-                    { new: true }
-                )
-                status = "done"
+                const appDate = new Date(Element.updatedAt)
+                const serverDate = new Date(idCheck.updatedAt)
+
+                if (appDate > serverDate) {
+                    const updatedClient = await Client.findByIdAndUpdate(idCheck._id, 
+                        {
+                            appId: Element.id,
+                            clientName: Element.client_name,
+                            phone: Element.phone,
+                            region: Element.region,
+                            prices: Element.prices,
+                            oldCredit: Element.old_credit,
+                            isCredit: is_credit,
+                            isFrigo: is_frigo,
+                            isPromo: is_promo,
+                            creditBon: Element.credit_bon,
+                            lastServe: Element.last_serve
+                        },
+                        { new: true }
+                    )
+                    status = "done"
+                } else {
+                    status = "done"
+                }
             } catch (err) {
                 status = err
             }
@@ -94,22 +101,29 @@ router.post("/datausers", async (req, res) => {
         const idCheck = await User.findOne({ appId: Element.id})
         if (idCheck != null) {
             try {
-                const updatedUser = await User.findByIdAndUpdate(idCheck._id, 
-                    {
-                        appId: Element.id,
-                        username: Element.username,
-                        email: `${Element.username}@gmail.com`,
-                        password: CryptoJS.AES.encrypt(
-                            Element.password,
-                            process.env.SECRET_KEY
-                            ).toString(),
-                        profilePic: Element.profilePic,
-                        camion: Element.camion,
-                        isAdmin: Element.isadmin,
-                    },
-                    { new: true }
-                )
-                status = "done"
+                const appDate = new Date(Element.updatedAt)
+                const serverDate = new Date(idCheck.updatedAt)
+
+                if (appDate > serverDate) {
+                    const updatedUser = await User.findByIdAndUpdate(idCheck._id, 
+                        {
+                            appId: Element.id,
+                            username: Element.username,
+                            email: `${Element.username}@gmail.com`,
+                            password: CryptoJS.AES.encrypt(
+                                Element.password,
+                                process.env.SECRET_KEY
+                                ).toString(),
+                            profilePic: Element.profilePic,
+                            camion: Element.camion,
+                            isAdmin: Element.isadmin,
+                        },
+                        { new: true }
+                    )
+                    status = "done"
+                } else {
+                    status = "done"
+                }
             } catch (err) {
                 status = err
             }
@@ -158,20 +172,30 @@ router.post("/dataregions", async (req, res) => {
 
     const dataFromApp = req.body.data
     var reutrnStatus
-
+ 
     async function insertData(Element) {
         var status = ""
         const idCheck = await Region.findOne({ appId: Element.id})
+       
         if (idCheck != null) {
             try {
-                const updatedRegion = await Region.findByIdAndUpdate(idCheck._id, 
-                    {
-                        appId: Element.id,
-                        regionName: Element.region_name
-                    },
-                    { new: true }
-                )
-                status = "done"
+                
+                const appDate = new Date(Element.updatedAt)
+                const serverDate = new Date(idCheck.updatedAt)
+
+                if (appDate > serverDate) {
+                    const updatedRegion = await Region.findByIdAndUpdate(idCheck._id, 
+                        {
+                            appId: Element.id,
+                            regionName: Element.region_name
+                        },
+                        { new: true }
+                    )
+                    status = "done"
+                } else {
+                    status = "done"
+                }
+                
             } catch (err) {
                 status = err
             }
@@ -218,18 +242,25 @@ router.post("/dataproducts", async (req, res) => {
         const idCheck = await Product.findOne({ appId: Element.id})
         if (idCheck != null) {
             try {
-                const updatedProduct = await Product.findByIdAndUpdate(idCheck._id, 
-                    {
-                        appId: Element.id,
-                        name: Element.name,
-                        price: Element.price,
-                        qty_par_one: Element.qty_par_one,
-                        image: Element.image,
-                        status: Element.status
-                    },
-                    { new: true }
-                )
-                status = "done"
+                const appDate = new Date(Element.updatedAt)
+                const serverDate = new Date(idCheck.updatedAt)
+
+                if (appDate > serverDate) {
+                    const updatedProduct = await Product.findByIdAndUpdate(idCheck._id, 
+                        {
+                            appId: Element.id,
+                            name: Element.name,
+                            price: Element.price,
+                            qty_par_one: Element.qty_par_one,
+                            image: Element.image,
+                            status: Element.status
+                        },
+                        { new: true }
+                    )
+                    status = "done"
+                } else {
+                    status = "done"
+                }
             } catch (err) {
                 status = err
             }
@@ -280,22 +311,29 @@ router.post("/dataorders", async (req, res) => {
         const idCheck = await Order.findOne({ appId: Element.id})
         if (idCheck != null) {
             try {
-                const updatedOrder = await Order.findByIdAndUpdate(idCheck._id, 
-                    {
-                        appId: Element.id,
-                        clientName: Element.client_name,
-                        clientId: Element.client_id,
-                        productListId: Element.product_list_id,
-                        totalToPay: Element.total_to_pay,
-                        verssi: Element.verssi,
-                        rest: Element.rest,
-                        date: Element.date,
-                        isCredit: Element.iscredit,
-                        isCheck: Element.is_check
-                    },
-                    { new: true }
-                )
-                status = "done"
+                const appDate = new Date(Element.updatedAt)
+                const serverDate = new Date(idCheck.updatedAt)
+
+                if (appDate > serverDate) {
+                    const updatedOrder = await Order.findByIdAndUpdate(idCheck._id, 
+                        {
+                            appId: Element.id,
+                            clientName: Element.client_name,
+                            clientId: Element.client_id,
+                            productListId: Element.product_list_id,
+                            totalToPay: Element.total_to_pay,
+                            verssi: Element.verssi,
+                            rest: Element.rest,
+                            date: Element.date,
+                            isCredit: Element.iscredit,
+                            isCheck: Element.is_check
+                        },
+                        { new: true }
+                    )
+                    status = "done"
+                } else {
+                    status = "done"
+                }
             } catch (err) {
                 status = err
             }
@@ -350,58 +388,65 @@ router.post("/dataorderproducts", async (req, res) => {
         const idCheck = await OrderedProduct.findOne({ appId: Element.id})
         if (idCheck != null) {
             try {
-                const updatedOrderedProduct = await OrderedProduct.findByIdAndUpdate(idCheck._id, 
-                    {
-                        appId: Element.id,
-                        orderId: Element.orderId,
-                        mini_qty: Element.mini_qty,
-                        mini_q_u: Element.mini_q_u,
-                        solo_qty: Element.solo_qty,
-                        solo_q_u: Element.solo_q_u,
-                        pot_qty: Element.pot_qty,
-                        pot_q_u: Element.pot_q_u,
-                        gini_qty: Element.gini_qty,
-                        gini_q_u: Element.gini_q_u,
-                        big_qty: Element.big_qty,
-                        big_q_u: Element.big_q_u,
-                        cornito_4_qty: Element.cornito_4_qty,
-                        cornito_4_q_u: Element.cornito_4_q_u,
-                        cornito_5_qty: Element.cornito_5_qty,
-                        cornito_5_q_u: Element.cornito_5_q_u,
-                        cornito_g_qty: Element.cornito_g_qty,
-                        cornito_g_q_u: Element.cornito_g_q_u,
-                        gofrito_qty: Element.gofrito_qty,
-                        gofrito_q_u: Element.gofrito_q_u,
-                        pot_v_qty: Element.pot_v_qty,
-                        pot_v_q_u: Element.pot_v_q_u,
-                        g8_qty: Element.g8_qty,
-                        g8_q_u: Element.g8_q_u,
-                        gold_qty: Element.gold_qty,
-                        gold_q_u: Element.gold_q_u,
-                        skiper_qty: Element.skiper_qty,
-                        skiper_q_u: Element.skiper_q_u,
-                        scobido_qty: Element.scobido_qty,
-                        scobido_q_u: Element.scobido_q_u,
-                        mini_scobido_qty: Element.mini_scobido_qty,
-                        mini_scobido_q_u: Element.mini_scobido_q_u,
-                        venezia_qty: Element.venezia_qty,
-                        venezia_q_u: Element.venezia_q_u,
-                        bf_400_q_u: Element.bf_400_q_u,
-                        bf_250_q_u: Element.bf_250_q_u,
-                        bf_230_q_u: Element.bf_230_q_u,
-                        bf_200_q_u: Element.bf_200_q_u,
-                        bf_150_q_u: Element.bf_150_q_u,
-                        buch_q_u: Element.buch_q_u,
-                        tarte_q_u: Element.tarte_q_u,
-                        mosta_q_u: Element.mosta_q_u,
-                        misso_q_u: Element.misso_q_u,
-                        juliana_q_u: Element.juliana_q_u,
-                        bac_5_q_u: Element.bac_5_q_u,
-                        bac_6_q_u: Element.bac_6_q_u
-                    },
-                    { new: true }
-                )
-                status = "done"
+                const appDate = new Date(Element.updatedAt)
+                const serverDate = new Date(idCheck.updatedAt)
+
+                if (appDate > serverDate) {
+                    const updatedOrderedProduct = await OrderedProduct.findByIdAndUpdate(idCheck._id, 
+                        {
+                            appId: Element.id,
+                            orderId: Element.orderId,
+                            mini_qty: Element.mini_qty,
+                            mini_q_u: Element.mini_q_u,
+                            solo_qty: Element.solo_qty,
+                            solo_q_u: Element.solo_q_u,
+                            pot_qty: Element.pot_qty,
+                            pot_q_u: Element.pot_q_u,
+                            gini_qty: Element.gini_qty,
+                            gini_q_u: Element.gini_q_u,
+                            big_qty: Element.big_qty,
+                            big_q_u: Element.big_q_u,
+                            cornito_4_qty: Element.cornito_4_qty,
+                            cornito_4_q_u: Element.cornito_4_q_u,
+                            cornito_5_qty: Element.cornito_5_qty,
+                            cornito_5_q_u: Element.cornito_5_q_u,
+                            cornito_g_qty: Element.cornito_g_qty,
+                            cornito_g_q_u: Element.cornito_g_q_u,
+                            gofrito_qty: Element.gofrito_qty,
+                            gofrito_q_u: Element.gofrito_q_u,
+                            pot_v_qty: Element.pot_v_qty,
+                            pot_v_q_u: Element.pot_v_q_u,
+                            g8_qty: Element.g8_qty,
+                            g8_q_u: Element.g8_q_u,
+                            gold_qty: Element.gold_qty,
+                            gold_q_u: Element.gold_q_u,
+                            skiper_qty: Element.skiper_qty,
+                            skiper_q_u: Element.skiper_q_u,
+                            scobido_qty: Element.scobido_qty,
+                            scobido_q_u: Element.scobido_q_u,
+                            mini_scobido_qty: Element.mini_scobido_qty,
+                            mini_scobido_q_u: Element.mini_scobido_q_u,
+                            venezia_qty: Element.venezia_qty,
+                            venezia_q_u: Element.venezia_q_u,
+                            bf_400_q_u: Element.bf_400_q_u,
+                            bf_250_q_u: Element.bf_250_q_u,
+                            bf_230_q_u: Element.bf_230_q_u,
+                            bf_200_q_u: Element.bf_200_q_u,
+                            bf_150_q_u: Element.bf_150_q_u,
+                            buch_q_u: Element.buch_q_u,
+                            tarte_q_u: Element.tarte_q_u,
+                            mosta_q_u: Element.mosta_q_u,
+                            misso_q_u: Element.misso_q_u,
+                            juliana_q_u: Element.juliana_q_u,
+                            bac_5_q_u: Element.bac_5_q_u,
+                            bac_6_q_u: Element.bac_6_q_u
+                        },
+                        { new: true }
+                    )
+                    status = "done"
+                } else {
+                    status = "done"
+                }
             } catch (err) {
                 status = err
             }
