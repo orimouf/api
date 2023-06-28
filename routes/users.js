05055647f5aa1d973bc45d78b4f1047d9ca5f2cd
Amir PC <orim.ouf@gmail.com>
2023-06-28 05:20:38 +0200

    update 1.0 28/06/2023

--- routes/users.js
+++ routes/users.js
@@ -58,19 +58,19 @@ router.get("/find/:id", async (req, res) => {
 })
 
 //GET ALL
-
-router.get("/", verify, async (req, res) => {
+// router.get("/", verify, async (req, res) => {
+router.get("/", async (req, res) => {
     const query = req.query.new
-    if(req.user.isAdmin) {
+    // if(req.user.isAdmin) {
         try {
             const users = query ? await User.find().sort({_id: -1}).limit(10) : await User.find()
             res.status(200).json(users)
         } catch (err) {
             res.status(500).json(err)
         }
-    } else {
-        res.status(500).json("you are not allowed to see all users!")
-    }
+    // } else {
+    //     res.status(500).json("you are not allowed to see all users!")
+    // }
 })
 
 //GET USER STATS
