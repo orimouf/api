fb6a1ad269419807e730b05ea7a4a9316936f31e
Amir PC <orim.ouf@gmail.com>
2023-08-05 05:11:01 +0200

    update 2.0 05/08/2023

--- routes/auth.js
+++ routes/auth.js
@@ -70,14 +70,14 @@ router.post("/login", async (req, res) => {
         )
 
         const { password, ...info } = user._doc
-
-        res.status(200).json({
-            status: 1,
-            message: "Login Successful",
-            data: {
-                ...info, accessToken
-            }
-        })
+        res.status(200).json({...info, accessToken})
+        // res.status(200).json({
+        //     status: 1,
+        //     message: "Login Successful",
+        //     data: {
+        //         ...info, accessToken
+        //     }
+        // })
     } catch(err) {
         res.status(500).json(err)
     }
